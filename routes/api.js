0a2a6dc8d9525ff3c746f163acb2c24092cf53c2
praveen005-github <b.g.praveen.pavan@gmail.com>
2021-03-27 00:44:36 +0530

    added .

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,31 @@ router.get('/userdata',(req,res) =>{
         console.log("Error in api.js1");
     });
 });
-
+router.post('/signin', (req, res) => {
+    user.find({
+        email: req.body.email,
+        password: req.body.password
+    }, (error, previousUser) => {
+        if (error) {
+            return res.send({
+                success: false,
+                message: 'Server error !!'
+            })
+        }
+        else if (previousUser.length != 1) {
+            return res.send({
+                success: false,
+                message: 'Invalid Email or Password !!'
+            })
+        }
+        else {
+            return res.send({
+                success: true,
+                message: 'signin successful !!'
+            })
+        }
+    })
+})
 router.post('/userdata',(req,res) =>{
     console.log('Body : ', req.body);
 
